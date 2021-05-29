411afc6d1f0ad2b8f99f644ac8704f878c5b1ac9
victorfaim <67723887+victorfaim@users.noreply.github.com>
2021-05-29 14:49:34 -0300

    Create Exercicio 18.c

---
+++ Exercicios do cap 3/Exercicio 18.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Faça um programa que receba uma temperatura em Celsius, calcule e mostre essa temperatura em
+Fahrenheit. Sabe-se que F = 180*(C + 32)/100.*/
+
+int main ()
+{
+    float celsiu, fahre;
+
+    printf ("Digite a temperatura em Celsius para conversao: ");
+    scanf ("%f%*c", &celsiu); // atribuindo valor a variavel
+
+    /*A Formula para o calculo é de (°F = °C × 1, 8 + 32 que em programa ficaria fahre = (celsiu*1,8)+32) 
+    mas no exercicio pede-se a formula 180*(C +32)/100 (entao seguimos de acordo com enunciado)*/
+    fahre= 180*(celsiu+32)/100;
+
+    printf ("\nA Temperatura %.1f celsius em Fahrenheit e de: %.1f\n\n", celsiu, fahre);
+
+
+
+    return 0;
+}
