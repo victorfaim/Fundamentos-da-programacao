4e2b5a420f966a234895f8e41658430aee9b0bea
victorfaim <67723887+victorfaim@users.noreply.github.com>
2021-05-29 14:59:52 -0300

    Update Exercicio 6.c

--- Exercicios do cap 3/Exercicio 6.c
+++ Exercicios do cap 3/Exercicio 6.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Um funcionário recebe um salário fixo mais 4% de comissão sobre as vendas. Faça um programa
+/* Victor Hugo Faim 
+victorfaim@hotmail.com
+
+Um funcionário recebe um salário fixo mais 4% de comissão sobre as vendas. Faça um programa
 que receba o salário fixo do funcionário e o valor de suas vendas, calcule e mostre a comissão e seu
 salário final. */
 
