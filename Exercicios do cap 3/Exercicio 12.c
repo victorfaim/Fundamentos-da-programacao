3fcbe5fbbf17acd2b57e9ec726e68a0fc0a8104b
victorfaim <67723887+victorfaim@users.noreply.github.com>
2021-05-29 14:59:00 -0300

    Update Exercicio 12.c

--- Exercicios do cap 3/Exercicio 12.c
+++ Exercicios do cap 3/Exercicio 12.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Faça um programa que receba o valor do salário mínimo e o valor do salário de um funcionário, calcule
+/* Victor Hugo Faim 
+victorfaim@hotmail.com
+
+Faça um programa que receba o valor do salário mínimo e o valor do salário de um funcionário, calcule
 e mostre a quantidade de salários mínimos que esse funcionário ganha */
 
 int main ()
