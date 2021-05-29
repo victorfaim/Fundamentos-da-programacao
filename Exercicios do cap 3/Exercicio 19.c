cc6df622d6d5ac117451408625abce9aa8af11d8
victorfaim <67723887+victorfaim@users.noreply.github.com>
2021-05-29 14:59:30 -0300

    Update Exercicio 19.c

--- Exercicios do cap 3/Exercicio 19.c
+++ Exercicios do cap 3/Exercicio 19.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
- /*Sabe-se que, para iluminar de maneira correta os cômodos de uma casa, para cada m2, deve-se usar 18 W
+
+/* Victor Hugo Faim 
+victorfaim@hotmail.com
+
+Sabe-se que, para iluminar de maneira correta os cômodos de uma casa, para cada m2, deve-se usar 18 W
 de potência. Faça um programa que receba as duas dimensões de um cômodo (em metros), calcule e mostre
 a sua área (em m2) e a potência de iluminação que deverá ser utilizada.*/
 
