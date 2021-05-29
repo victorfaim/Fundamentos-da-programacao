fa9b514b173a7048aa7b32df4ba292a182144997
victorfaim <67723887+victorfaim@users.noreply.github.com>
2021-05-29 12:13:59 -0300

    Create Exercicio 2.c

---
+++ Exercicios do cap 3/Exercicio 2.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Faça um programa que receba três números, calcule e mostre a multiplicação desses números. */
+int main ()
+{
+    int num1, num2, num3;
+    int resultado;
+    
+    printf ("Digite um numero: ");
+    scanf ("%d%*c", &num1);
+    printf ("Digite um segundo: ");
+    scanf ("%d%*c", &num2);
+    printf ("Digite um terceiro: ");
+    scanf ("%d%*c", &num3);
+
+    resultado = num1*num2*num3; 
+
+    printf ("O resultado da multiplicacao dos 3 numeros e de: %d", resultado);
+
+
+
+    return 0;
+}
