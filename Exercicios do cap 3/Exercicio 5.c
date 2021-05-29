f9d2b271b9cbd95bf1f3f84c4d5b67ea55f478d4
victorfaim <67723887+victorfaim@users.noreply.github.com>
2021-05-29 14:59:47 -0300

    Update Exercicio 5.c

--- Exercicios do cap 3/Exercicio 5.c
+++ Exercicios do cap 3/Exercicio 5.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Faça um programa que receba o preço de um produto, calcule e mostre o novo preço, sabendo-se
+/* Victor Hugo Faim 
+victorfaim@hotmail.com
+
+Faça um programa que receba o preço de um produto, calcule e mostre o novo preço, sabendo-se
 que este sofreu um desconto de 10%. 
 */
 int main ()
