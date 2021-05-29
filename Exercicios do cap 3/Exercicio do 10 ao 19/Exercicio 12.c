a97117b5d83809be462cb47596fb169240ee2c5b
victorfaim <67723887+victorfaim@users.noreply.github.com>
2021-05-29 15:10:47 -0300

    Create Exercicio 12.c

---
+++ Exercicios do cap 3/Exercicio do 10 ao 19/Exercicio 12.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Victor Hugo Faim 
+victorfaim@hotmail.com
+
+Faça um programa que receba o valor do salário mínimo e o valor do salário de um funcionário, calcule
+e mostre a quantidade de salários mínimos que esse funcionário ganha */
+
+int main ()
+{
+    float salario, sala_min;
+    float qntd;
+
+    printf ("Digite o salario minimo atual: ");
+    scanf ("%f%*c", &sala_min);
+
+    printf ("Digite o seu salario atual: ");
+    scanf ("%f%*c", &salario);
+
+    qntd = salario/sala_min;
+
+    printf ("Voce recebe um total de %.2f salarios minimos!", qntd);
+    
+
+
+    return 0;
+}
