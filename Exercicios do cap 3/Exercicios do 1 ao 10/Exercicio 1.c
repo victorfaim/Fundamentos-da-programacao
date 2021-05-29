efa3f9746becbdca711492ec079d678e1f1d1ac6
victorfaim <67723887+victorfaim@users.noreply.github.com>
2021-05-29 15:03:17 -0300

    Create Exercicio 1.c

---
+++ Exercicios do cap 3/Exercicios do 1 ao 10/Exercicio 1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Victor Hugo Faim 
+victorfaim@hotmail.com
+
+ "Faça um programa que receba dois números, calcule e mostre a subtração do primeiro número pelo
+segundo"*/
+int main()
+{
+    int num1, num2; // Declaração das variaveis do tipo inteiro
+    int resultado;
+
+    printf ("Digite um numero: ");
+    scanf ("%d%*c", &num1); //Atribuindo valor a variavel ja limpando o buffer do teclado com o %*c
+    printf ("Digite um segundo numero: ");
+    scanf ("%d%*c", &num2);
+
+    resultado=num1-num2; //Fazendo a conta proposta no exercicio ja atribuindo-a na variavel
+
+    printf ("A subtracao do primeiro numero pelo segundo e de %d", resultado);
+
+     
+    return 0;
+}
