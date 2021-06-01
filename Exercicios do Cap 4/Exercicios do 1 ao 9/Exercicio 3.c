dd5167d037437b1f1a8b10e311ea22032a0d4816
victorfaim <67723887+victorfaim@users.noreply.github.com>
2021-06-01 20:17:00 -0300

    Update Exercicio 3.c

--- Exercicios do Cap 4/Exercicios do 1 ao 9/Exercicio 3.c
+++ Exercicios do Cap 4/Exercicios do 1 ao 9/Exercicio 3.c
@@ -3,11 +3,22 @@
 
 /* Victor Hugo Faim 
 victorfaim@hotmail.com
- */
+ Faça um programa que receba dois números e mostre o menor*/
 
 int main ()
 {
-
+    int n1, n2;
+    // entrada de dados
+    printf ("Digite dois numeros: ");
+    scanf ("%d%*c", &n1);
+    scanf ("%d%*c", &n2);
+    // Comparações e saidas de dados.
+    if (n1>n2){
+        printf ("O numero maior e o %d", n1);
+    }
+    else {
+        printf ("O numero maior e o %d", n2);
+    }
 
 
 
